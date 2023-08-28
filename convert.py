5f7ca0f1df001574e6bb726e83a76da214c9e301
Gus Smith <guscomps@gmail.com>
2023-08-28 13:50:08 -0700

    Update

--- convert.py
+++ convert.py
@@ -16,17 +16,20 @@
 
 import os
 import glob
+from pathlib import Path
+import tempfile
 from typing import List, Optional, Tuple
 import bs4
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 from fairseq.checkpoint_utils import load_model_ensemble_and_task_from_hf_hub
 from fairseq.models.text_to_speech.hub_interface import TTSHubInterface
+import requests
 import scipy
 import numpy as np
 import sys
 
 
-def main(arxiv_id: Optional[str] = None, remove_ranges: List[Tuple[int,int]] = []):
+def main(output_filepath: str,arxiv_id: Optional[str] = None, remove_ranges: List[Tuple[int, int]] = []):
     # For now, expect that arxiv_id is set. In the future, would be nice to support other ways to get papers.
     assert arxiv_id is not None, "arxiv_id must be set."
     paper_id = arxiv_id
@@ -55,19 +58,19 @@ def main(arxiv_id: Optional[str] = None, remove_ranges: List[Tuple[int,int]] = [
     os.system(f"tar -xzf {tar_gz_file}")
 
     # Convert to HTML.
-    get_sentences_from_tex(paper_id=paper_id, remove_ranges=remove_ranges)
+    sentences = get_sentences_from_tex(paper_id=paper_id, remove_ranges=remove_ranges)
 
     # Go back. Up one level.
     os.chdir("..")
 
     # Convert to wav.
-    convert_sentences_to_wav(paper_id=paper_id)
+    convert_sentences_to_wav(sentences=sentences, output_filepath=output_filepath)
 
     # Remove temp folder.
     os.system(f"rm -rf temp")
 
 
-def get_sentences_from_tex(paper_id: str, remove_ranges: List[Tuple[int,int]] = []):
+def get_sentences_from_tex(paper_id: str, remove_ranges: List[Tuple[int, int]] = []):
     # Find all the .tex files in the temp folder.
     tex_files = glob.glob(f"*.tex")
 
@@ -84,7 +87,7 @@ def get_sentences_from_tex(paper_id: str, remove_ranges: List[Tuple[int,int]] = 
     # the lower line numbers. This assumes non-overlapping ranges.
     remove_ranges = reversed(sorted(remove_ranges, key=lambda x: x[0]))
 
-    for (start, end) in remove_ranges:
+    for start, end in remove_ranges:
         with open(documentclass_file, "r") as f:
             lines = f.readlines()
         lines = lines[:start] + lines[end:]
@@ -97,7 +100,16 @@ def get_sentences_from_tex(paper_id: str, remove_ranges: List[Tuple[int,int]] = 
     # Load the .html file with BeautifulSoup4.
     with open(f"{paper_id}.html", "r") as f:
         html = f.read()
+
+    return _clean_html(html, id=paper_id)
+
+
+def _clean_html(html, id: str):
     soup = BeautifulSoup(html, "html.parser")
+
+    # Clean up comments.
+    for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
+        comment.extract()
 
     # Cleanup. Rigorouslly, we want to remove a lot of tags.
     for element in soup.find_all("span", class_="citation"):
@@ -128,18 +140,23 @@ def get_sentences_from_tex(paper_id: str, remove_ranges: List[Tuple[int,int]] = 
         element.decompose()
     for element in soup.find_all("tbody"):
         element.decompose()
+    for element in soup.find_all("footer"):
+        element.decompose()
+
+    for element in soup.find_all("div", class_="paper__text"):
+        element.unwrap()
 
     # Elements to unwrap (i.e. remove the tag, but keep the contents).
-    for tag in ["span", "strong", "em", "i", "b", "li", "code", "ul", "ol"]:
+    for tag in ["span", "strong", "em", "i", "b", "li", "code", "ul", "ol" "div"]:
         for element in soup.find_all(tag):
             element.unwrap()
 
     # Write the .html file back.
-    with open(f"{paper_id}_cleaned.html", "w") as f:
+    with open(f"{id}_cleaned.html", "w") as f:
         f.write(soup.prettify())
 
     # Read that .html file back line by line.
-    with open(f"{paper_id}_cleaned.html", "r") as f:
+    with open(f"{id}_cleaned.html", "r") as f:
         lines = f.readlines()
 
     # Convert to sentences. Go through the lines
@@ -149,21 +166,23 @@ def get_sentences_from_tex(paper_id: str, remove_ranges: List[Tuple[int,int]] = 
         if line.startswith("<"):
             # Opening tags that we expect.
             if (
-                line.startswith("<p")
-                or line.startswith("<h1")
-                or line.startswith("<h2")
-                or line.startswith("<h3")
-                or line.startswith("<h4")
+                line.strip().startswith("<p")
+                or line.strip().startswith("<h1")
+                or line.strip().startswith("<h2")
+                or line.strip().startswith("<h3")
+                or line.strip().startswith("<h4")
+                or line.strip().startswith("<article")
             ):
                 pass
 
             # Closing tags that we expect.
             elif (
-                line.startswith("</p>")
-                or line.startswith("</h1>")
-                or line.startswith("</h2>")
-                or line.startswith("</h3>")
-                or line.startswith("</h4>")
+                line.strip().startswith("</p>")
+                or line.strip().startswith("</h1>")
+                or line.strip().startswith("</h2>")
+                or line.strip().startswith("</h3>")
+                or line.strip().startswith("</h4>")
+                or line.strip().startswith("</article>")
             ):
                 accumumlated_sentence = accumumlated_sentence.replace("\n", " ")
 
@@ -188,7 +207,7 @@ def get_sentences_from_tex(paper_id: str, remove_ranges: List[Tuple[int,int]] = 
             accumumlated_sentence += line
 
     # Write to a file.
-    with open(f"{paper_id}_sentences.txt", "w") as f:
+    with open(f"{id}_sentences.txt", "w") as f:
         for sentence in sentences:
             if sentence.strip() != "":
                 f.write(sentence + "\n")
@@ -197,10 +216,7 @@ def get_sentences_from_tex(paper_id: str, remove_ranges: List[Tuple[int,int]] = 
     return sentences
 
 
-def convert_sentences_to_wav(paper_id: str):
-    # Load lines from the .txt file.
-    with open(f"temp/{paper_id}_sentences.txt", "r") as f:
-        sentences = f.readlines()
+def convert_sentences_to_wav(sentences: List[str], output_filepath: str):
 
     # Load the model.
     print("Loading TTS model...")
@@ -241,14 +257,32 @@ def convert_sentences_to_wav(paper_id: str):
     full_wave_file = np.array(full_wave_file, dtype=np.float32)
 
     # Save the generated audio to a file.
-    wav_path = f"{paper_id}.wav"
+    wav_file = tempfile.NamedTemporaryFile(suffix=".wav")
+    wav_path = wav_file.name
     print(f"Saving {wav_path}")
     scipy.io.wavfile.write(wav_path, rate, full_wave_file)
     print("Done.")
 
     print("Converting to mp3...")
-    os.system(f"ffmpeg -y -i {wav_path} {paper_id}.mp3")
+    os.system(f"ffmpeg -y -i {wav_path} {output_filepath}")
     print("Done.")
+
+
+def convert_with_papertohtml(url: str, output_filepath: str):
+    downloaded = tempfile.NamedTemporaryFile(suffix=".pdf")
+    print(f"Downloading paper {url}...")
+    os.system(f"wget {url} -O {downloaded.name}")
+    response = requests.post(
+        "https://papertohtml.org/upload", files={"file": open(downloaded.name, "rb")}
+    )
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    assert len(soup.find_all("div", class_="paper__text")) == 1, "There should be only one div.paper__text tag."
+    soup = soup.find_all("div", class_="paper__text")[0]
+    sentences= _clean_html(str(soup), id="tmp")
+    convert_sentences_to_wav(sentences=sentences, output_filepath=output_filepath)
+
+    exit(0)
 
 
 # Call the main method.
@@ -260,10 +294,28 @@ if __name__ == "__main__":
     parser.add_argument(
         "--arxiv_id",
         type=str,
-        # TODO(@gussmith23): In the future, it would be nice to support papers
-        # from other sources.
+        required=False,
+        default=None,
+        help=(
+            "The arxiv ID of the paper to convert. This is preferable to",
+            " --paper_url.",
+        ),
+    )
+    parser.add_argument(
+        "--paper_url",
+        type=str,
+        required=False,
+        default=None,
+        help=(
+            "The URL of the paper to convert. One of --arxiv_id or --paper_url"
+            " is required."
+        ),
+    )
+    parser.add_argument(
+        "--output_filepath",
+        type=str,
         required=True,
-        help="The arxiv ID of the paper to convert.",
+        help="Path of the output MP3 file."
     )
     parser.add_argument(
         "--remove_ranges",
@@ -277,9 +329,20 @@ if __name__ == "__main__":
             " each (start,end) pair. Assumes ranges are"
             " non-overlapping; things will break if this is not the case!"
             " Only removes lines from the top-level file."
+            " Only applies to --arxiv_id."
         ),
     )
 
     args = parser.parse_args()
 
-    main(arxiv_id=args.arxiv_id, remove_ranges=ast.literal_eval(args.remove_ranges))
+    assert (
+        args.arxiv_id is not None or args.paper_url is not None
+    ), "One of --arxiv_id or --paper_url is required."
+
+    output_filepath = Path(args.output_filepath).absolute()
+
+    if args.arxiv_id is not None:
+        main(arxiv_id=args.arxiv_id, remove_ranges=ast.literal_eval(args.remove_ranges), output_filepath=output_filepath)
+
+    if args.paper_url is not None:
+        convert_with_papertohtml(args.paper_url, output_filepath=output_filepath)
